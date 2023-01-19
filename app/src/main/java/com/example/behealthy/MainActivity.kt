6127ab195629c59074b5266f4c3a96db0306d36d
kim <kmichel.jae@gmail.com>
2023-01-18 23:34:57 -0300

    Set navigation

--- app/src/main/java/com/example/behealthy/MainActivity.kt
+++ app/src/main/java/com/example/behealthy/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.behealthy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import com.example.behealthy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/behealthy/ui/signin/SignInFragment.kt
@@ -0,0 +1,23 @@
+package com.example.behealthy.ui.signin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.behealthy.databinding.FragmentSigninBinding
+
+class SignInFragment: Fragment() {
+
+    private lateinit var binding: FragmentSigninBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSigninBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
